cfc2a565bbd60bc9aecc7b6a2a8f7a29d774f4e9
Rudraksh.Chourey <Rudraksh.Chourey@ENCDAPPUNLT0462>
2025-10-07 17:00:47 +0530

    performanceProject

---
+++ PerformanceAssignment/src/main/java/com/performanceAssignment/PerformanceAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.performanceAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceAssignmentApplication.class, args);
+	}
+
+}
---
+++ PerformanceAssignment/src/test/java/com/performanceAssignment/PerformanceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performanceAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
